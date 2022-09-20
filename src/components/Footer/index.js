c9b95e67720aab99ea7230ead839ac18f124ee2b
Finn Stitcher <foundfootage@protonmail.com>
2022-09-19 21:03:32 -0700

    basic layout

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+
+function Footer() {
+    return (
+        <footer>
+            <a>GitHub</a>
+
+            <a>LinkedIn</a>
+
+            <a>Stack Overflow</a>
+        </footer>
+    )
+};
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <div>
+                <h2>Finn Stitcher</h2>
+            </div>
+
+            <nav>
+                <a href="#about">About Me</a>
+
+                <a href="#projects">Projects</a>
+
+                <a href="#resume">Resume</a>
+
+                <a href="#contact">Contact</a>
+            </nav>
+        </header>
+    )
+};
+
+export default Header;
