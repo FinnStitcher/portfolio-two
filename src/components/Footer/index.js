02cf01c2c41e49701b0938060e1cd1f84fed7f24
Finn Stitcher <foundfootage@protonmail.com>
2022-09-30 13:33:06 -0700

    fontawesome icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faStackOverflow } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
     return (
         <footer className="d-flex justify-content-center gap-3 p-3 bg-two">
-            <a href="https://github.com/FinnStitcher">GitHub</a>
+            <a href="https://github.com/FinnStitcher">
+                <FontAwesomeIcon icon={faGithub} />
+            </a>
 
-            <a href="https://www.linkedin.com/in/diana-phillips-518b46238/">LinkedIn</a>
+            <a href="https://www.linkedin.com/in/diana-phillips-518b46238/">
+                <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+
+            <a href="https://stackoverflow.com">
+                <FontAwesomeIcon icon={faStackOverflow} />
+            </a>
         </footer>
     )
 };
