b4f8ec5382a3c1219dd4843e5d049e4249245ec0
Finn Stitcher <foundfootage@protonmail.com>
2023-01-11 13:33:19 -0800

    updated projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,34 +2,35 @@ import React from 'react';
 
 const projectsArray = [
     {
-        id: "apply",
-        title: "APPly",
+        id: "wildfound",
+        title: "Wildfound",
         technologies: [
-            "GraphQL",
-            "Apollo Express",
-            "React",
-            "React Router",
-            "React Bootstrap",
-            "JSON Web Tokens",
-            "Mongoose",
-            "MongoDB"
+            "MySQL",
+            "Node.js",
+            "Express.js",
+            "Handlebars.js",
+            "Amazon RDS",
+            "Sequelize",
+            "Tailwind",
         ],
-        description: "Finding a new job can be hectic and confusing - that's why APPly is here, to help you keep track of where you are in your search.",
-        githubLink: "https://github.com/sarameadows/APPly"
+        description: "Explore a conservation database through layers of geographic and taxonomic division.",
+        githubLink: "https://github.com/FinnStitcher/wildfound",
+        deployedLink: "https://wildfound-production.up.railway.app/"
     },
 
     {
         id: "lightbox",
         title: "Lightbox",
         technologies: [
-            "Express",
-            "Express Session",
             "MySQL",
+            "Node.js",
+            "Express.js",
+            "Express Session",
+            "Handlebars.js",
+            "Amazon EC2",
             "Sequelize",
             "Bcrypt",
-            "Handlebars.js",
-            "Luxon",
-            "Amazon EC2"
+            "Luxon"
         ],
         description: "Lightbox is a blogging website with a minimalist UI and aesthetic, so you can focus on what's important.",
         githubLink: 'https://github.com/FinnStitcher/lightbox',
@@ -40,81 +41,88 @@ const projectsArray = [
         id: "libre-library",
         title: "Libre Library",
         technologies: [
-            "React",
-            "Express",
-            "Apollo Express",
+            "MongoDB",
             "GraphQL",
-            "MongoDB"
+            "Node.js",
+            "Express.js",
+            "React",
+            "Apollo Express",
+            "Mongoose"
         ],
         description: "Search for your favorite books and save them to your personal, digital library, accessible only to you.",
         githubLink: "https://github.com/FinnStitcher/libre-library",
         deployedLink: "https://libre-library-production.up.railway.app/"
     },
 
-    {
-        id: "wildfound",
-        title: "Wildfound",
-        technologies: [
-            "Express",
-            "Handlebars.js",
-            "MySQL",
-            "Sequelize",
-            "Tailwind",
-            "Amazon RDS"
-        ],
-        description: "Explore a conservation database through layers of geographic and taxonomic division.",
-        githubLink: "https://github.com/FinnStitcher/wildfound",
-        deployedLink: "https://wildfound-production.up.railway.app/"
-    },
+    // {
+    //     id: "weather",
+    //     title: "Weather Dashboard",
+    //     technologies: [
+    //         "jQuery",
+    //         "Moment.js",
+    //         "OpenWeather's OneCall and Geocoding APIs"
+    //     ],
+    //     description: "A streamlined weather app that provides the current weather and a five-day forecast for any city in the United States.",
+    //     githubLink: "https://github.com/FinnStitcher/weather-dashboard",
+    //     deployedLink: "https://finnstitcher.github.io/weather-dashboard/"
+    // },
+
+    // {
+    //     id: "team-profile",
+    //     title: "Team Profile Generator",
+    //     technologies: [
+    //         "Node.js",
+    //         "Inquirer"
+    //     ],
+    //     description: "A command-line application with complex promise-based architecture. Give it your team's info, and it'll generate an HTML page for you.",
+    //     githubLink: "https://github.com/FinnStitcher/team-profile-generator",
+    //     deployedLink: null
+    // },
 
     {
-        id: "weather",
-        title: "Weather Dashboard",
+        id: "apply",
+        title: "APPly",
         technologies: [
-            "jQuery",
-            "Moment.js",
-            "OpenWeather's OneCall and Geocoding APIs"
-        ],
-        description: "A streamlined weather app that provides the current weather and a five-day forecast for any city in the United States.",
-        githubLink: "https://github.com/FinnStitcher/weather-dashboard",
-        deployedLink: "https://finnstitcher.github.io/weather-dashboard/"
-    },
-
-    {
-        id: "team-profile",
-        title: "Team Profile Generator",
-        technologies: [
+            "MongoDB",
+            "GraphQL",
             "Node.js",
-            "Inquirer"
+            "Express.js",
+            "React",
+            "React Router",
+            "React Bootstrap",
+            "Apollo Express",
+            "Mongoose",
+            "JSON Web Tokens"
         ],
-        description: "A command-line application with complex promise-based architecture. Give it your team's info, and it'll generate an HTML page for you.",
-        githubLink: "https://github.com/FinnStitcher/team-profile-generator",
-        deployedLink: null
+        description: "Finding a new job can be hectic and confusing - that's why APPly is here, to help you keep track of where you are in your search.",
+        githubLink: "https://github.com/sarameadows/APPly"
     },
 
-    {
-        id: "shelter",
-        title: "Endless Pawsibilities",
-        technologies: [
-            "MySQL",
-            "Sequelize",
-            "Express.js",
-            "Handlebars",
-            "JavaScript"
-        ],
-        description: "A mock animal-management tool for shelter employees. I worked on this with two teammates and created the database and assisted with frontend styles and form control.",
-        githubLink: "https://github.com/sarameadows/project2"
-    },
+    // {
+    //     id: "shelter",
+    //     title: "Endless Pawsibilities",
+    //     technologies: [
+    //         "MySQL",
+    //         "Sequelize",
+    //         "Express.js",
+    //         "Handlebars",
+    //         "JavaScript"
+    //     ],
+    //     description: "A mock animal-management tool for shelter employees. I worked on this with two teammates and created the database and assisted with frontend styles and form control.",
+    //     githubLink: "https://github.com/sarameadows/project2"
+    // },
 
     {
         id: "deep-thoughts",
         title: "Deep Thoughts",
         technologies: [
+            "MongoDB",
+            "Node.js",
+            "Express.js",
             "React",
             "React Router",
-            "MongoDB",
             "Mongoose",
-            "JWT authentication"
+            "JSON Web Tokens"
         ],
         description: "A social media website wherein users share and react to \"thoughts\" and make friends.",
         githubLink: "https://github.com/FinnStitcher/deep-thoughts-two",
